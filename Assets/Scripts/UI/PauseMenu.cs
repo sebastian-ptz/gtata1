8277fb305579faf114a08716b4ae9e352c9fd2aa
Robin <r.jaspers18@gmail.com>
2020-05-08 00:45:37 +0200

    finally 2.0 >.<

---
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] private Button ResumeButton;
+    [SerializeField] private Button RestartButton;
+    [SerializeField] private Button BackToMenuButton;
+
+    private void Start()
+    {
+        ResumeButton.onClick.AddListener(HandleResumeClicked);
+        RestartButton.onClick.AddListener(HandleRestartClicked);
+        BackToMenuButton.onClick.AddListener(BacktoMenuClicked);
+    }
+
+    void HandleResumeClicked()
+    {
+        GameManager.Instance.TogglePause();
+    }
+
+    void HandleRestartClicked()
+    {
+        GameManager.Instance.RestartGame();
+    }
+
+    void BacktoMenuClicked()
+    {
+        GameManager.Instance.BackToMenu();
+    }
+}
