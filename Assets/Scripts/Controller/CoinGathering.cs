6cef75bd6b32893ebfc62198c067501e433c323e
Robin <r.jaspers18@gmail.com>
2020-05-08 21:12:03 +0200

    scene 2&4

---
+++ Assets/Scripts/Controller/CoinGathering.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CoinGathering : MonoBehaviour
+{
+    public Text score;
+    public Text highScore;
+    private int number = 0;
+
+    private void Start()
+    {
+        highScore.text = PlayerPrefs.GetInt("Highscore", 0).ToString();
+    }
+
+    public void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == "Coin(Clone)")
+        {
+            Destroy(col.gameObject);
+            number += 1;
+            score.text = number.ToString();
+
+            if (number > PlayerPrefs.GetInt("Highscore", 0))
+            {
+                PlayerPrefs.SetInt("Highscore", number);
+                highScore.text = number.ToString();
+            }
+        }
+    }
+
+    public void Reset()
+    {
+        PlayerPrefs.DeleteAll();
+    }
+}
